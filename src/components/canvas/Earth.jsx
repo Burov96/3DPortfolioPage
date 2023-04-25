0675e651c5589a28e80b0c5fd923ddfeb375327b
Teodor <burov96@gmail.com>
2023-04-25 19:26:48 +0200

    new format

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -7,7 +7,7 @@ import CanvasLoader from '../Loader'
 
 
 const Earth = () => {
-  const earth=useGLTF('/planet/scene.gltf')
+  const earth=useGLTF('/planet/scene-transformed.glb')
   return (
   <primitive
   object={earth.scene}
